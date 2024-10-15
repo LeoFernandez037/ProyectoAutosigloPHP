904b55cc000e77ca93643af9fd17b4d228a4b389
ShallyC20 <carlymar204@gmail.com>
2024-10-15 01:54:54 -0400

    ConeccionPHOi

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <li><a href="#">Confección de autos</a></li>
     <li><a href="#">Servicios</a></li>
     <li><a href="#">Contacto</a></li>
-    <li><a href="Cotizacion.html">Cotizacion</a></li>
+    <li><a href="Cotizacion.php">Cotizacion</a></li>
     <li><a href="login.html">Inicio de sesion</a></li>
     <li><a href="Registro.html">Registro</a></li>
   </ul>
